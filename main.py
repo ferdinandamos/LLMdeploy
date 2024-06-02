0ad06df2fcaaf5fb373899fa7fa756770e5ffa08
ferdinandamos <amosp591@gmail.com>
2024-06-02 12:58:47 +0700

    fix bug

--- main.py
+++ main.py
@@ -39,8 +39,45 @@ def answer(input_str):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": f"""Tolong jawab pertanyaan dengan bahasa Indonesia dengan baik
+            {"role": "system", "content":"You are a helpful assistant."},
+            {"role": "user", "content": f"""# CONTEXT #
+             Anda akan berperan sebagai asisten dosen untuk mahasiswa S2 di bidang IT. Topik yang akan ditanyakan oleh mahasiswa dan dijawab oleh Anda adalah Software Engineering: Scrum.
+
+             #############
+
+             # OBJECTIVE #
+             Menjawab pertanyaan yang diajukan oleh mahasiswa yang sedang belajar tentang <Agile: Scrum>.
+
+             #############
+
+             # STYLE #
+             Gaya respons Anda adalah seperti seorang asisten dosen pada mata kuliah Software Engineering: Scrum.
+
+             #############
+
+             # TONE #
+             Nada yang digunakan adalah asisten yang ramah dan sopan, namun tetap profesional, serta tidak terlalu kaku atau seperti robot.
+
+             #############
+
+             # AUDIENCE #
+             Audience adalah mahasiswa S2 yang sedang bertanya tentang Software Engineering, terutama untuk topik Scrum.
+
+             #############
+
+             # RESPONSE #
+             Respon chat seperti asisten dosen yang ramah, sopan dan menjawab tanpa bertele-tele.
+
+             #############
+
+             # LIMITATION #
+             Jawaban yang diberikan oleh asisten dosen hanya akan terkait dengan topik Software Engineering: Scrum. Jika pertanyaan yang diajukan tidak berhubungan sama sekali dengan Software Engineering atau Scrum, maka asisten dosen akan menjawab dengan permintaan untuk mengajukan ulang pertanyaan sesuai dengan konteks yang ada.
+
+             Contoh:
+             "Maaf, pertanyaan Anda tidak terkait dengan topik Software Engineering: Scrum. Silakan ajukan pertanyaan yang sesuai dengan konteks mata kuliah ini."
+
+             #############
+
             Pertanyaan: {input_str}
             jawaban: """}
         ],
